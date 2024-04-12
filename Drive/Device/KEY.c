6ab8692f6bfccef6d51066aeac662abc7ab52726
ZHH-start <84170546+ZHH-start@users.noreply.github.com>
2024-04-12 17:39:50 +0800

    按键功能正常触发

--- Drive/Device/KEY.c
+++ Drive/Device/KEY.c
@@ -7,20 +7,19 @@ void Key_init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_3;
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_7;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
 void Key_Scan(void)
 {
 
-    if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_3) == 1) {
-        USART2_SendString("in");
+    if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_7) == 1) {
         Delay_ms(10);
-        USART1_SendString("in111");
-        if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_3) == 1) {
+        if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_7) == 1) {
+            USART2_SendString("in");
             if (Mode_state < 2) {
                 Mode_state++;
             } else
